584f4f2590ec2c4e84e7516b96359de1892ef59a
tanjimakib <tanjim.akib@portonics.com>
2022-06-13 01:02:21 +0600

    Unit test added for models/tweet findAll & findAndCountAll

--- __tests__/unit/models/tweet.test.js
+++ __tests__/unit/models/tweet.test.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef */
 
 // Load Cutom Dependencies
+const { Op } = require('sequelize');
 const { tweet } = require('../../../models');
 const { generateFakeTweet } = require('../../../utils/faker');
 
@@ -14,6 +15,88 @@ describe('models/tweet', () => {
       expect(result).toHaveProperty('userId'),
       expect(result).toHaveProperty('createdAt'),
       expect(result).toHaveProperty('updatedAt'),
+    ]);
+  });
+  it('Should find own tweets(All) of user', async () => {
+    const results = await tweet.findAll({ where: { userId: 1 } });
+    return Promise.all([expect(results).toBeInstanceOf(Object)]);
+  });
+  it('Should find own tweets(All) of user for type all', async () => {
+    const results = await tweet.findAll({
+      where: {
+        userId: 1,
+        isPublic: {
+          [Op.in]: [0, 1],
+        },
+      },
+    });
+    return Promise.all([expect(results).toBeInstanceOf(Object)]);
+  });
+  it('Should find own tweets(Private) of user', async () => {
+    const results = await tweet.findAll({
+      where: {
+        userId: 1,
+        isPublic: 0,
+      },
+    });
+    return Promise.all([expect(results).toBeInstanceOf(Object)]);
+  });
+  it('Should find own tweets(Public) of user', async () => {
+    const results = await tweet.findAll({
+      where: {
+        userId: 1,
+        isPublic: 0,
+      },
+    });
+    return Promise.all([expect(results).toBeInstanceOf(Object)]);
+  });
+  it('Should find own tweets(All) of user with pagination info', async () => {
+    const results = await tweet.findAndCountAll({ where: { userId: 1 } });
+    return Promise.all([
+      expect(results).toBeInstanceOf(Object),
+      expect(results).toHaveProperty('rows'),
+      expect(results).toHaveProperty('count'),
+    ]);
+  });
+  it('Should find own tweets(All) of user for type all with pagination info', async () => {
+    const results = await tweet.findAndCountAll({
+      where: {
+        userId: 1,
+        isPublic: {
+          [Op.in]: [0, 1],
+        },
+      },
+    });
+    return Promise.all([
+      expect(results).toBeInstanceOf(Object),
+      expect(results).toHaveProperty('rows'),
+      expect(results).toHaveProperty('count'),
+    ]);
+  });
+  it('Should find own tweets(Private) of user with pagination info', async () => {
+    const results = await tweet.findAndCountAll({
+      where: {
+        userId: 1,
+        isPublic: 0,
+      },
+    });
+    return Promise.all([
+      expect(results).toBeInstanceOf(Object),
+      expect(results).toHaveProperty('rows'),
+      expect(results).toHaveProperty('count'),
+    ]);
+  });
+  it('Should find own tweets(Public) of user with pagination info', async () => {
+    const results = await tweet.findAndCountAll({
+      where: {
+        userId: 1,
+        isPublic: 0,
+      },
+    });
+    return Promise.all([
+      expect(results).toBeInstanceOf(Object),
+      expect(results).toHaveProperty('rows'),
+      expect(results).toHaveProperty('count'),
     ]);
   });
 });
