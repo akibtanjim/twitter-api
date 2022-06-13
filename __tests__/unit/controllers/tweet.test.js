22160e4340ba8b9d60b3e57c38fcd0ec5dea08aa
tanjimakib <tanjim.akib@portonics.com>
2022-06-13 06:12:21 +0600

    Unit test added for controllers/tweet getTweetsForNewsFeed method

--- __tests__/unit/controllers/tweet.test.js
+++ __tests__/unit/controllers/tweet.test.js
@@ -1,6 +1,10 @@
 /* eslint-disable no-undef */
 
-const { saveTweet, getSelfTweets } = require('../../../controllers');
+const {
+  saveTweet,
+  getSelfTweets,
+  getTweetsForNewsFeed,
+} = require('../../../controllers');
 const { generateFakeTweet } = require('../../../utils');
 
 describe('controllers/tweet', () => {
@@ -162,6 +166,133 @@ describe('controllers/tweet', () => {
       const req = mock.mockRequest();
       const res = mock.mockResponse();
       await getSelfTweets(req, res);
+      expect(res.status).toHaveBeenCalledTimes(1);
+      expect(res.json).toHaveBeenCalledTimes(1);
+    });
+    it('Should fetch news feed tweets successfully', async () => {
+      const mock = {
+        mockRequest: () => {
+          const req = {
+            headaers: {
+              authorization:
+                'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibmFtZSI6ImFkbWluIiwicm9sZSI6ImFkbWluIiwicmVmcmVzaCI6dHJ1ZSwiaWF0IjoxNjUxMzUwMzEyLCJleHAiOjE2NTM5NzgzMTJ9.JZNMkx2XVHRBktek1wor1edI0x5KtR3ESLLmRcv-9Kc',
+            },
+            query: {
+              page: 1,
+            },
+          };
+          req.query = jest.fn().mockReturnValue(req);
+          req.params = jest.fn().mockReturnValue(req);
+          return req;
+        },
+
+        mockResponse: () => {
+          const res = {
+            status: 'success',
+            data: {
+              totalItems: 1710,
+              items: [
+                {
+                  id: 10160,
+                  description:
+                    'Ut explicabo fugit ad libero aliquam est incidunt impedit aut.',
+                  userId: 18,
+                  isPublic: 1,
+                  createdAt: '2022-06-12T19:44:42.000Z',
+                  updatedAt: '2022-06-12T19:44:42.000Z',
+                },
+                {
+                  id: 10161,
+                  description:
+                    'Explicabo voluptatem est cumque laborum nam aliquid.',
+                  userId: 50,
+                  isPublic: 1,
+                  createdAt: '2022-06-12T19:44:42.000Z',
+                  updatedAt: '2022-06-12T19:44:42.000Z',
+                },
+                {
+                  id: 10163,
+                  description: 'Ad a commodi voluptatem.',
+                  userId: 24,
+                  isPublic: 1,
+                  createdAt: '2022-06-12T19:44:42.000Z',
+                  updatedAt: '2022-06-12T19:44:42.000Z',
+                },
+                {
+                  id: 10174,
+                  description: 'Recusandae quia reprehenderit voluptas.',
+                  userId: 2,
+                  isPublic: 1,
+                  createdAt: '2022-06-12T19:44:42.000Z',
+                  updatedAt: '2022-06-12T19:44:42.000Z',
+                },
+                {
+                  id: 10177,
+                  description:
+                    'Blanditiis fuga temporibus quae doloremque animi aspernatur nulla.',
+                  userId: 16,
+                  isPublic: 1,
+                  createdAt: '2022-06-12T19:44:42.000Z',
+                  updatedAt: '2022-06-12T19:44:42.000Z',
+                },
+                {
+                  id: 10179,
+                  description:
+                    'Nihil voluptatibus assumenda pariatur et officia est rerum ad aut.',
+                  userId: 2,
+                  isPublic: 1,
+                  createdAt: '2022-06-12T19:44:42.000Z',
+                  updatedAt: '2022-06-12T19:44:42.000Z',
+                },
+                {
+                  id: 10181,
+                  description: 'Enim in deserunt illo.',
+                  userId: 17,
+                  isPublic: 1,
+                  createdAt: '2022-06-12T19:44:42.000Z',
+                  updatedAt: '2022-06-12T19:44:42.000Z',
+                },
+                {
+                  id: 10190,
+                  description: 'Omnis enim dignissimos qui facilis sit est.',
+                  userId: 18,
+                  isPublic: 1,
+                  createdAt: '2022-06-12T19:44:42.000Z',
+                  updatedAt: '2022-06-12T19:44:42.000Z',
+                },
+                {
+                  id: 10192,
+                  description:
+                    'Aut voluptatum illo est eveniet totam fuga ullam placeat necessitatibus.',
+                  userId: 10,
+                  isPublic: 1,
+                  createdAt: '2022-06-12T19:44:42.000Z',
+                  updatedAt: '2022-06-12T19:44:42.000Z',
+                },
+                {
+                  id: 10193,
+                  description:
+                    'Quaerat quisquam aliquam autem sunt similique ad aliquam et.',
+                  userId: 16,
+                  isPublic: 1,
+                  createdAt: '2022-06-12T19:44:42.000Z',
+                  updatedAt: '2022-06-12T19:44:42.000Z',
+                },
+              ],
+              totalPages: 171,
+              currentPage: 1,
+            },
+            message: 'Successfully fetched all news feed tweets!',
+          };
+          res.json = jest.fn().mockReturnValue(res);
+          res.status = jest.fn().mockReturnValue(res);
+          res.json = jest.fn().mockReturnValue(res);
+          return res;
+        },
+      };
+      const req = mock.mockRequest();
+      const res = mock.mockResponse();
+      await getTweetsForNewsFeed(req, res);
       expect(res.status).toHaveBeenCalledTimes(1);
       expect(res.json).toHaveBeenCalledTimes(1);
     });
